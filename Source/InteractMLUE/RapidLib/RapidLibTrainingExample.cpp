b0e4f985b85310a85209180991532c53561c5521
Sam Swain <sam@apparance.uk>
2021-08-19 14:55:00 +0100

    Remove old (project level) RapidLib integration

--- Source/InteractMLUE/RapidLib/RapidLibTrainingExample.cpp
+++
@@ -1,7 +0,0 @@
-#include "RapidLibTrainingExample.h"
-
-InteractML::FRapidLibTrainingExample::FRapidLibTrainingExample(TArray<double> inputs, TArray<double> outputs)
-{
-	input = inputs;
-	output = outputs;
-}
